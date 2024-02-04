39f79edda2b7f74b0dda9ec29cbbaecc4e79f329
olzhic <142490031+olzhic@users.noreply.github.com>
2024-02-04 19:41:28 +0600

    ya obosralsya

--- polycephaly/blog/urls.py
+++ polycephaly/blog/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.PostView.as_view()),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('review/<int:pk>/', views.AddComments.as_view(), name = 'add_comments'),
-    path('review/<int:pk>/', views.AddComments.as_view(), name = 'add_like')
+    path('review/<int:pk>/', views.AddComments.as_view(), name = 'add_like'),
+    path('register/', views.register, name = 'register'),
 ]
--- polycephaly/blog/views.py
+++ polycephaly/blog/views.py
@@ -3,8 +3,26 @@ from django.views.generic.base import View
 from .models import Post
 from .form import CommentsForm
 from .models import Like
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
 
 class PostView(View):
 
@@ -13,13 +31,13 @@ class PostView(View):
         return render(request, 'blog.html', {'post_list':posts})
     
 class PostDetail(View):
-
+    @login_required
     def get(self, request, pk):
         post = Post.objects.get(id = pk)
         return render(request, 'blog_detail.html', {'post':post})
     
 class AddComments(View):
-
+    @login_required
     def post(self, request, pk):
         form = CommentsForm(request.POST)
         if form.is_valid():
